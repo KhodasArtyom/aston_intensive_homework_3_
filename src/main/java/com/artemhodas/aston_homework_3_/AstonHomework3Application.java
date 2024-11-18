81d37900b837b1d71e2a36d62635a4716fbb6163
ArtyomKhodas <a.Khodas1601@gmail.com>
2024-11-18 18:50:21 +0300

    initial_commit

---
+++ src/main/java/com/artemhodas/aston_homework_3_/AstonHomework3Application.java
@@ -0,0 +1,13 @@
+package com.artemhodas.aston_homework_3_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonHomework3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonHomework3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/artemhodas/aston_homework_3_/AstonHomework3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemhodas.aston_homework_3_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonHomework3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
