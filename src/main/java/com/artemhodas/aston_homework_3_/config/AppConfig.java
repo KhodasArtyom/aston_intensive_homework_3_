27024e7a0d88e601bafc1b2289cd1bf4e1c07f7a
ArtyomKhodas <a.Khodas1601@gmail.com>
2024-11-19 20:05:45 +0300

    add config file

--- src/main/java/com/artemhodas/aston_homework_3_/config/AppConfig.java
+++ src/main/java/com/artemhodas/aston_homework_3_/config/AppConfig.java
@@ -1,4 +1,71 @@
 package com.artemhodas.aston_homework_3_.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Configuration
 public class AppConfig {
+
+    private Properties loadProperties() {
+        Properties properties = new Properties();
+        try (InputStream input = new PathMatchingResourcePatternResolver()
+                .getResource("application.properties")
+                .getInputStream()) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        Properties properties = loadProperties();
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(properties.getProperty("db.url"));
+        dataSource.setUsername(properties.getProperty("db.username"));
+        dataSource.setPassword(properties.getProperty("db.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPackagesToScan("ваш.package.model"); // Укажите пакет, где находятся ваши сущности
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        factoryBean.setJpaProperties(hibernateProperties());
+        return factoryBean;
+    }
+
+    private java.util.Properties hibernateProperties() {
+        java.util.Properties properties = new java.util.Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.show_sql", "true"); //
+        properties.put("hibernate.hbm2ddl.auto", "validate");
+        return properties;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
 }
